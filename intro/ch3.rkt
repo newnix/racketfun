d6317cda384fb3192266b071ae2dbea8d4341a90
newnix <newnix>
2019-09-24 02:56:02 +0000

    Finish 3.3 Examples
    	* intro/ch3.rkt
    		- Finish examples through ch 3.3 "characters"

--- intro/ch3.rkt
+++ intro/ch3.rkt
@@ -33,3 +33,39 @@
 (time (round (sigma (lambda (x) (/ 1 x)) 1 2000)))
 (printf "Timing for (round (sigma (lambda (x) (/ 1.0 x)) 1 2000))\n")
 (time (round (sigma (lambda (x) (/ 1.0 x)) 1 2000)))
+
+;; the = procedure checks for numerical equality, if given both inexact and exact 
+;; numbers, it'll convert both numbers to exact before comparing.
+;; the eqv? and equal? functions will consider both exactness and quality
+(printf "Running (= 1 1.0) -> ~s\n" (= 1 1.0))
+(printf "Running (eqv? 1 1.0) -> ~s\n" (eqv? 1 1.0))
+
+;; 3.3: Characters
+;; A Racket character corresponds to a unicode scalar value,
+;; this is a 21-bit value that maps to some notion of a "glyph" 
+;; or a printable character or portion of a character in the Uincode standard.
+;; basically, any single latin character or common Chinese character can fit this definition.
+;; A printable character will typically be printed as #\${CHAR}, while unprintable ones will be #\u${VAL}
+;; when using the integer->char procedure.
+(printf "\nBasic int->char conversions:\n")
+(printf "(integer->char 65) -> ~s\n" (integer->char 65))
+(printf "(char->integer A) -> ~s\n" (char->integer #\A))
+;; This is probably supposed to be lambda, but the codepoint doesn't seems right
+(printf "#\\u03BB -> ~s\n" #\u03BB)
+
+;; There are several available procedures for doing tests and transformations
+;; on characters, though some will only work as expected when presented as strings
+(printf "\nRunning basic character conversion functions:\n")
+(printf "(char-alphabetic? #\\A) -> ~s\n" (char-alphabetic? #\A))
+(printf "(char-numeric? #\\0) -> ~s\n" (char-numeric? #\0))
+(printf "(char-whitespace? #\\newline) -> ~s\n" (char-whitespace? #\newline))
+(printf "(char-downcase #\\A) -> ~s\n" (char-downcase #\A))
+
+;; The char=? function compares two or more characters, and char-ci=? 
+;; does the same, ignoring case. eqv? and equal? operate identically on characters;
+;; char=? should be used when specifically declaring the values should be treated as characters
+(printf "\nRunning some character equivalence tests:\n")
+(printf "(char=? #\\a #\\A) -> ~s\n" (char=? #\a #\A))
+(printf "(char-ci=? #\\a #\\A) -> ~s\n" (char-ci=? #\a #\A))
+(printf "(eqv? #\\a (char-downcase #\\A)) -> ~s\n" (eqv? #\a (char-downcase #\A)))
+(printf "(eqv? #\\a #\\A) -> ~s\n" (eqv? #\a #\A))
