06cdcb8a5f407d18bc971a5d14e897acb8311a55
newnix <newnix>
2019-09-26 03:09:42 +0000

    Begin Education Assistant Module
    	* intro/whatis.rkt
    		- Begin the table to check for a given identifier's type for help 
    		  in better understanding the program structure in the REPL

---
+++ intro/whatis.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+;; Create a simple filter function to identify the type of a given argument
+;; only really useful in the repl
+(define (whatis i)
+	; define an internal function for printing variables with a given type string
+	(define (id v)
+		(if (string? v)
+				(printf "\"~a\" is a(n) ~a\n" i v)
+				(printf "~s is not a string!\n" (quote v))))
+	(cond
+		[(integer? i) (id "integer")]
+		[(boolean? i) (id "boolean")]
+		[(string? i) (id "string")]
+		[(identifier? i) (id "identifier")]
+		[(procedure? i) (id "procedure")]
+		[(vector? i) (id "vector")]
+		[(byte? i) (id "byte")]
+		))
