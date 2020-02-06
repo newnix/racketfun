ebc0a5efa2a7b5f52c2ed41fd37fd59ac3a5a98c
newnix <newnix>
2020-02-06 22:16:05 +0000

    Exploring Ways to Get Path Info
    	* stuff/pathinfo.rkt
    	  - Create (hash-if-exists) procedure, exploring how to create a hash table of path info

--- stuff/pathinfo.rkt
+++ stuff/pathinfo.rkt
@@ -3,6 +3,8 @@
 (provide pathbins)
 
 (define env (current-environment-variables))
+(define path (getpath))
+(define bins (pathlsts))
 
 ; This returns the value of your $PATH as a string
 (define (getpath-str env)
@@ -21,3 +23,10 @@
 ; Get a list of directory contents
 (define (pathlsts)
 	(map directory-list (getpath)))
+
+; Generate a hash if the lists aren't empty
+(define (hash-if-exists pathlst binslst)
+	(lambda () 
+		(if (empty? pathlst)
+				'()
+				(hash (first pathlst) (first binslst)))))
