01450753d74145404389daa55e5736941eb929ca
newnix <newnix>
2020-03-13 20:04:56 +0000

    Add Code Examples, Update Pathinfo
    	* intro/ch4.rkt
    		- Add code examples for 4.9.1 and 4.9.2
    	
    	* stuff/pathinfo.rkt
    		- Simplify (unpath lst)
    		- Begin using the new (unpath lst) form in (hash-if-exists) form

--- stuff/pathinfo.rkt
+++ stuff/pathinfo.rkt
@@ -6,32 +6,30 @@
 
 ; This returns the value of your $PATH as a string
 (define (getpath-str env)
-		(if (environment-variables? env)
-				(bytes->string/utf-8 (environment-variables-ref env #"PATH"))
-				'()))
+    (if (environment-variables? env)
+        (bytes->string/utf-8 (environment-variables-ref env #"PATH"))
+        '()))
 
 ; This returns the value of $PATH as a list of strings
 (define (getpath)
-	(string-split (getpath-str env) ":"))
+  (string-split (getpath-str env) ":"))
 
 ; Build a list of hashes for the directories in $PATH
 (define (pathbins)
-	(map hash (getpath) (pathlsts)))
+  (map hash (getpath) (pathlsts)))
 
 ; Get a list of directory contents
 (define (pathlsts)
-	(map directory-list (getpath)))
+  (map directory-list (getpath)))
 
 ; Generate a hash if the lists aren't empty
 (define (hash-if-exists pathlst binslst)
-	(lambda () 
-		(if (empty? pathlst)
-				'()
-				(hash (first pathlst) (first binslst)))))
+  (lambda () 
+    (if (empty? pathlst)
+        '()
+        (hash (first pathlst) (first binslst)))))
 
 (define (unpath lst)
-  (lambda ()
-    (if (empty? lst)
-      '()
-      (path->string (first lst))))
-  (unpath (rest lst)))
+  (if (empty? lst)
+    '()
+    (map path->string lst)))
